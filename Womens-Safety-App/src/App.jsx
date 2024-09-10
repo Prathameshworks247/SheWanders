e40c30ff170846d903b386a7f4f4e84e37ed1d59
Prathamesh Patil <prathameshworks247@gmail.com>
2024-09-11 03:06:06 +0530

    Login basics

--- Womens-Safety-App/src/App.jsx
+++ Womens-Safety-App/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Login from './components/Login';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <Login/>
     </>
   )
 }
---
+++ Womens-Safety-App/src/components/Login.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import womenpic from '../assets/protection.png'
+export default function Login() {
+    return(
+        <div className="login-card mb-3 mt-3 text-dark shadow">
+            <div className=""><img src={womenpic} alt="profile" className="img"/></div>
+            <div className="div-2 col-6">
+            <h1>Login</h1>
+            <form action="" className="form">
+            <div class="mb-3 mt-3">
+                {/* <label for="email" className="form-label">Email:</label> */}
+                <input type="email" className="form-control" id="email" placeholder="Enter email" name="email"/>
+            </div>
+            <div className="mb-3">
+                {/* <label for="pwd" class="form-label">Password:</label> */}
+                <input type="password" className="form-control" id="pwd" placeholder="Enter password" name="pswd"/>
+            </div>
+            <div className="form-check mb-3">
+                <label className="form-check-label">
+                <input className="form-check-input" type="checkbox" name="remember"/> Remember me
+                </label>
+            </div>
+            <button type="submit" className="btn shadow">Submit</button>
+            </form>
+            </div>
+
+            
+        </div>
+
+    )
+}
