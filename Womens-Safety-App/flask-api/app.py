ddad3f30ffd17a82428eb4cd9f9c0823807762a3
ibeginnernoob <adheilgupta@gmail.com>
2024-11-04 16:47:58 +0530

    cleanup

--- Womens-Safety-App/flask-api/app.py
+++ Womens-Safety-App/flask-api/app.py
@@ -1,6 +1,10 @@
 from flask import Flask,request,jsonify
 from flask_cors import CORS,cross_origin
 import os
+import cv2
+from deepface import DeepFace
+import io
+import numpy as np
 
 app = Flask(__name__)
 CORS(app)
@@ -16,10 +20,23 @@ def hello_world():
 
 def verify():
     image_file=request.files['image']
-    save_path=os.path.join('./images', image_file.filename)
-    image_file.save(save_path)
+    # save_path=os.path.join('./images', image_file.filename)
+    # image_file.save(save_path)
 
-    return "Hello World!"
+    in_memory_file=io.BytesIO()
+    image_file.save(in_memory_file)
+    data=np.frombuffer(in_memory_file.getvalue(), dtype=np.uint8)
+
+    img_array=cv2.imdecode(data, cv2.IMREAD_COLOR)
+
+    analysis=DeepFace.analyze(img_path=img_array, actions=['gender'])
+    gender=analysis[0]['dominant_gender']
+
+    if gender.lower()=='woman':
+        return jsonify({"message": "Gender verified as female"}), 200
+    else:
+        return jsonify({"message": "Gender verified as male"}), 500
+
 
 if __name__=="__main__":
     app.run(port=3000,debug=True)
